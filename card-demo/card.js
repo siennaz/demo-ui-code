04736f855d13aa06be7bc2be330fb60c10b8b63c
Sienna Zhao <uxsienna@gmail.com>
2023-12-07 13:44:35 +1100

    updates

--- card-demo/card.js
+++ card-demo/card.js
@@ -1,7 +1,8 @@
 let idx = 0; 
 const imgs = ["url(https://images.unsplash.com/photo-1483366774565-c783b9f70e2c?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)", 
 "url(https://jll-global-gdim-res.cloudinary.com/image/upload/t_test-au-property-detail-web/v1689819925/AUS_CRM/a86f8e7e-406a-408a-abad-96d9c5fc5b3f_ONEBarangaroo005.jpg)", 
-"url(https://images.unsplash.com/photo-1575025699516-4735ac10c265?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)"];
+"url(https://images.unsplash.com/photo-1575025699516-4735ac10c265?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+"url(winwin.jpeg)"];
 
 
 function copyProjectID(){
